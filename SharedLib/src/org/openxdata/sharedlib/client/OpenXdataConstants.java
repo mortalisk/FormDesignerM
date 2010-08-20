029ea4b729762ccc1e0decca250cc61186561f21
cattabanks@gmail.com <cattabanks@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-08-20 16:14:02 +0000

    Refactoring package names to include the sharedlib naming convention
    git-svn-id: https://trac.openxdata.org/svn/openxdata/Forms/trunk@2751 6cddc80d-1c40-44cd-927a-2be6005847d2

---
+++ SharedLib/src/org/openxdata/sharedlib/client/OpenXdataConstants.java
@@ -0,0 +1,22 @@
+package org.openxdata.sharedlib.client;
+
+
+/**
+ * Constants used by the form designer and runner.
+ * 
+ *  www.openxdata.org - Licensed as written in license.txt and original sources of this file and its authors are found in sources.txt.
+ *
+ */
+public class OpenXdataConstants {
+	
+	public static final String UNITS = "px";
+	
+	/** The separator between the xforms xml and javascript source in a combined xml document for a form. */
+	public static final String OPENXDATA_FORMDEF_JAVASCRIPT_SRC_SEPARATOR = " PURCFORMS_FORMDEF_JAVASCRIPT_SRC_SEPARATOR ";
+
+	/** The separator between the xforms xml and layout xml in a combined xml document for a form. */
+	public static final String OPENXDATA_FORMDEF_LAYOUT_XML_SEPARATOR = " PURCFORMS_FORMDEF_LAYOUT_XML_SEPARATOR ";
+	
+	/** The separator between the xforms xml and locale xml in a combined xml document for a form. */
+	public static final String OPENXDATA_FORMDEF_LOCALE_XML_SEPARATOR = " PURCFORMS_FORMDEF_LOCALE_XML_SEPARATOR ";
+}
---
+++ SharedLib/src/org/openxdata/sharedlib/client/controller/OpenFileDialogEventListener.java
@@ -0,0 +1,20 @@
+package org.openxdata.sharedlib.client.controller;
+
+
+
+/**
+ * This interface is implemented by those interested in listening to
+ * events during the opening of a file from the file system.
+ * 
+ *  www.openxdata.org - Licensed as written in license.txt and original sources of this file and its authors are found in sources.txt.
+ *
+ */
+public interface OpenFileDialogEventListener {
+	
+	/**
+	 * Called when the file contents have been retrieved.
+	 * 
+	 * @param contents the file contents.
+	 */
+	public void onSetFileContents(String contents);
+}

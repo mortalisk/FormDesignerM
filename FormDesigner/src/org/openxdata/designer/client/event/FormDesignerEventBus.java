3130229c6891a15ff991ea94e7d9bda5c876cf67
benjamingruba@gmail.com <benjamingruba@gmail.com@6cddc80d-1c40-44cd-927a-2be6005847d2>
2010-12-15 02:20:08 +0000

    Instantiating the bus statically seems to cause exceptions, so I have changed this to a traditional singleton registry with an instance field for the bus and private constructor.
    git-svn-id: https://trac.openxdata.org/svn/openxdata/Forms/branches/usm-project@3553 6cddc80d-1c40-44cd-927a-2be6005847d2

--- FormDesigner/src/org/openxdata/designer/client/event/FormDesignerEventBus.java
+++ FormDesigner/src/org/openxdata/designer/client/event/FormDesignerEventBus.java
@@ -6,16 +6,29 @@ import com.google.gwt.event.shared.SimpleEventBus;
 /* This is a simple registry that provides access to the shared event bus 
  * for the form designer. Our hope is that the event bus will ultimately be 
  * injected, and this class will only be a shim to support early refactoring.
- * 
- * Because this class should be temporary, and I can't foresee a need to inherit 
- * from it or mock the event bus, it is not a full-blown singleton.
  */
 
 public class FormDesignerEventBus {
 	
-	private static EventBus eventBus = new SimpleEventBus();
+	private static FormDesignerEventBus instance = null;
+	private EventBus eventBus;
+	
+	private FormDesignerEventBus(){
+		this.eventBus = new SimpleEventBus();
+	}
+	
+	public EventBus getEventBus(){
+		return eventBus;
+	}
+	
+	public static FormDesignerEventBus getInstance(){
+		if (instance == null)
+			instance = new FormDesignerEventBus();
+		
+		return instance;
+	}
 	
 	public static EventBus getBus(){
-		return eventBus;
+		return getInstance().getEventBus();
 	}
 }
